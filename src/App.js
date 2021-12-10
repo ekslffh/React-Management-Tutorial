5f3287f564476beba5d859a5e1389a429892c002
ekslffh <ekslffh@naver.com>
2021-12-10 15:02:49 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,49 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Customer from "./components/Customer";
+
+const customers = [
+  {
+    id: 1,
+    image: "https://placeimg.com/64/64/1",
+    name: "나성민",
+    birthday: "990201",
+    gender: "남자",
+    job: "대학생",
+  },
+  {
+    id: 2,
+    image: "https://placeimg.com/64/64/2",
+    name: "전미도",
+    birthday: "931222",
+    gender: "여자",
+    job: "배우",
+  },
+  {
+    id: 3,
+    image: "https://placeimg.com/64/64/3",
+    name: "허영만",
+    birthday: "640505",
+    gender: "남자",
+    job: "만화가",
+  },
+];
 
 function App() {
   return (
-    <div className="gray-background">
-      <img src={logo} alt="logo" />
-      <h2>Let's develop management system!</h2>
+    <div>
+      {customers.map((customer) => {
+        return (
+          <Customer
+            key={customer.id}
+            id={customer.id}
+            image={customer.image}
+            name={customer.name}
+            birthday={customer.birthday}
+            gender={customer.gender}
+            job={customer.job}
+          />
+        );
+      })}
     </div>
   );
 }
---
+++ src/components/Customer.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+class Customer extends React.Component {
+  render() {
+    return (
+      <div>
+        <CustomerProfile {...this.props} />
+        <CustomerInfo {...this.props} />
+      </div>
+    );
+  }
+}
+
+class CustomerInfo extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>{this.props.birthday}</p>
+        <p>{this.props.gender}</p>
+        <p>{this.props.job}</p>
+      </div>
+    );
+  }
+}
+
+class CustomerProfile extends React.Component {
+  render() {
+    return (
+      <div>
+        <img src={this.props.image} alt="profile" />
+        <h2>
+          {this.props.name}({this.props.id})
+        </h2>
+      </div>
+    );
+  }
+}
+
+export default Customer;
